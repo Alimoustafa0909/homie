94f4f6845ba9fe78bf84f8d2ff85316b7dc40f95
farah220 <68689314+farah220@users.noreply.github.com>
2023-10-31 00:38:32 +0200

    users

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -17,28 +18,13 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $guarded = ['password_confirmation'];
+    protected $hidden = ['password'];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+
 }
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'dashboard', 'as' => 'dashboard.', 'middleware' => ['web','auth:admins']],function (){
 Route::view('/','dashboard.index');
 Route::resource('admins','AdminController');
+Route::resource('users','UserController');
 Route::resource('contact','ContactController')->except(['store','create','edit','update']);
 
 Route::post('/logout','AdminController@logout')->name('logout');
