1357093e0a854147a7fc06d87160ef950fe42a95
farah220 <68689314+farah220@users.noreply.github.com>
2023-11-01 19:07:03 +0200

    asset

--- resources/views/web/partials/foot.blade.php
+++ resources/views/web/partials/foot.blade.php
@@ -1,8 +1,8 @@
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC3nDHy1dARR-Pa_2jjPCjvsOR4bcILYsM"></script>
 <!-- Plugins JS -->
-<script src="assets/js/plugins.js"></script>
+<script src="{{asset('assets/js/plugins.js')}}"></script>
 <!-- Map Active JS -->
-<script src="assets/js/maplace-active.js"></script>
+<script src="{{asset('assets/js/maplace-active.js')}}"></script>
 <!-- Main JS -->
-<script src="assets/js/main.js"></script>
+<script src="{{asset('assets/js/main.js')}}"></script>
 @stack('scripts')
--- resources/views/web/partials/head.blade.php
+++ resources/views/web/partials/head.blade.php
@@ -5,15 +5,15 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon.ico')}}">
 
     <!-- CSS
 	============================================ -->
 
     <!-- Plugins CSS -->
-    <link rel="stylesheet" href="assets/css/plugins.css">
+    <link rel="stylesheet" href="{{asset('assets/css/plugins.css')}}">
     <!-- Main Style CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 
 </head>
 @stack('styles')
